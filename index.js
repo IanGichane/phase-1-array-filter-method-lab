6ae6daa5be4d1c9d702406433aafc73888ff5d20
Ian <gichaneian@gmail.com>
2023-09-27 10:06:40 +0300

    Completed Filter Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    name = name.toLowerCase(); 
+    let matching = drivers.filter(x => x.toLowerCase() === name);
+    return matching;
+ }
+
+ console.log(findMatching(drivers, 'Bobby')); 
+
+
+ function fuzzyMatch(drivers,firstLetters){
+   let sameFirstLetter = drivers.filter(driver => driver.startsWith(firstLetters))
+   return sameFirstLetter
+
+}
+console.log(fuzzyMatch(drivers, 'Sa')); 
+
+function matchName(drivers,name){
+   let nameMatching = drivers.filter(y => y.name=== name); 
+   return nameMatching;
+
+}
+matchName(drivers,'Bobby')
